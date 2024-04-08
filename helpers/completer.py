7d8104063491ecc19ac9fe487a1ceaf6910fbddd
Julia <email96123@gmail.com>
2024-04-08 02:15:02 -0600

    Add completer function to helpers/completer.py

---
+++ helpers/completer.py
@@ -0,0 +1,9 @@
+from parsers import completion_words
+
+def completer(text, state):
+    options = [word for word in completion_words if word.startswith(text)]
+
+    if state < len(options):
+        return options[state]
+    else:
+        return None
