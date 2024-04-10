3675ce8f9c6c06efb84f9e73072f4b771ee5664e
Julia <email96123@gmail.com>
2024-04-10 15:25:38 -0600

    Update version number in setup.py to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='cheesebox',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     description='semantic calculations',
     long_description=long_description,
@@ -17,7 +17,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "cheesebox = main:main",
+            "cheesebox = cheesebox.main:main",
         ],
     },
 )
