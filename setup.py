6bbf34eba7743ab02c1905b54e2fa92f77ed73c3
Julia <email96123@gmail.com>
2024-04-10 13:40:16 -0600

    add README

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='cheesebox',
     version='0.1',
     packages=find_packages(),
     description='semantic calculations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='dejmedus',
     author_email='hi@juliab.dev',
     url='https://github.com/dejmedus/cheesebox',
