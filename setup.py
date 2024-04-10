bf759391629b2ed270657f6f1860d3974f2ea81f
Julia <email96123@gmail.com>
2024-04-10 14:02:03 -0600

    change version num

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='cheesebox',
-    version='0.1',
+    version='0.0.1',
     packages=find_packages(),
     description='semantic calculations',
     long_description=long_description,
