d5a3f3960cf3d386dfa8b6b52434d46b04640225
Julia <email96123@gmail.com>
2024-04-16 15:32:59 -0600

    add fraction word replacers

--- cheesebox/replacers/__init__.py
+++ cheesebox/replacers/__init__.py
@@ -4,8 +4,10 @@ This module imports all shared replacers: numbers, symbols
 
 from .numbers import number_replacers
 from .symbols import symbol_replacers
+from .fractions import fraction_replacers
 
 shared_replacers = {
     **number_replacers,
-    **symbol_replacers
+    **symbol_replacers,
+    **fraction_replacers
 }
