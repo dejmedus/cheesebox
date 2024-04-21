2d626ef961fe621b0223077dcb277d9f4849213a
Julia <email96123@gmail.com>
2024-04-21 16:57:06 -0600

    remove readline autocomplete

--- cheesebox/main.py
+++ cheesebox/main.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python3
 
 import os
-import readline
 
 from cheesebox.parsers import *
-from cheesebox.helpers.completer import completer
 from cheesebox.helpers.messages import *
-
-
-if 'libedit' in readline.__doc__:
-    readline.parse_and_bind("bind ^I rl_complete")
-else:
-    readline.parse_and_bind("tab: complete")
+from cheesebox.helpers.extended_input import extended_input
 
 
 def main():
-    readline.set_completer(completer)
     os.system('clear')
     welcome_message()
-    
 
     while True:
         try:
-            expression = input("> ")
+            expression = extended_input()
             cleaned_expression = expression.lower().strip()
 
             if cleaned_expression == 'exit' or cleaned_expression == 'quit()':
