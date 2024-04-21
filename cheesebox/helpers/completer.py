4959b5a2bd2815292ef3b81e41de81e5b34aef47
Julia <email96123@gmail.com>
2024-04-21 16:57:31 -0600

    remove completer

--- cheesebox/helpers/completer.py
+++
@@ -1,9 +0,0 @@
-from cheesebox.parsers import completion_words
-
-def completer(text, state):
-    options = [word for word in completion_words if word.startswith(text)]
-
-    if state < len(options):
-        return options[state]
-    else:
-        return None
