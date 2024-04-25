169f47ee931c91919b24bbdd6772faf1c3776f68
Julia <email96123@gmail.com>
2024-04-24 22:12:39 -0600

    convert into larger unit when possible

--- cheesebox/parsers/measurements.py
+++ cheesebox/parsers/measurements.py
@@ -4,6 +4,16 @@ from fractions import Fraction
 from cheesebox.parsers.parser import ParserData
 from cheesebox.helpers.exception_handler import exception_handler
 
+unit_conversion = {
+    "tsp": {"next_unit": "tbsp", "conversion_factor": 3},
+    "tbsp": {"next_unit": "c", "conversion_factor": 16},
+    "c": {"next_unit": "qt", "conversion_factor": 4},
+    "qt": {"next_unit": "gal", "conversion_factor": 4},
+    "oz": {"next_unit": "lb", "conversion_factor": 16},
+    "g": {"next_unit": "kg", "conversion_factor": 1000},
+    "ml": {"next_unit": "l", "conversion_factor": 1000},
+}
+
 def find_unit(expression):
     regex = r"(\d+|)\s*(" + "c|tbsp|tsp|lb|oz|g|ml|l|pt|qt|gal" + r")\b"
     match = re.search(regex, expression)
@@ -17,6 +27,10 @@ def calculate(expression: str) -> str:
     unit = find_unit(expression)
     expression = expression.replace(unit, "")
     result = eval(expression)
+
+    while unit in unit_conversion and result >= unit_conversion[unit]["conversion_factor"]:
+        result /= unit_conversion[unit]["conversion_factor"]
+        unit = unit_conversion[unit]["next_unit"]
 
     whole_part = int(result)
     fraction_part = Fraction(result - whole_part).limit_denominator(16)
@@ -39,8 +53,10 @@ measurement_parser = ParserData(
         "cup": "c",
         "tablespoon": "tbsp",
         "tablespoons": "tbsp",
+        "tbsps": "tbsp",
         "teaspoon": "tsp",
         "teaspoons": "tsp",
+        "tsps": "tsp",
         "pound": "lb",
         "pounds": "lb",
         "ounce": "oz",
